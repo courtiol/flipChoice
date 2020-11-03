18d0c1e8323e697ca62e72fea51dfe44cfec9ee2
Alexandre Courtiol <alexandre.courtiol@gmail.com>
2020-11-03 21:10:59 +0100

    Partially restore missing documentation

---
+++ R/data.R
@@ -0,0 +1,15 @@
+#' The experiment question from the Eggs.Q project.
+#' @docType data
+#' @name eggs.data
+#' @usage data(eggs)
+#' @format A data.frame with 380 rows and 200 variables.
+NULL
+
+#' Choice Design from Huber and Zwerina
+#' A dataset containing a choice model experimental design chosen to minimize D-error used in an example in Huber and Zwerina (1996).
+#' @references Joel Huber and Klaus Zwerina (1996). The Importance of Utility Balance in Efficient Choice Designs. Journal of MArketing Research.
+#' @docType data
+#' @name hz.design
+#' @usage data(hz.design)
+#' @format A matrix with 27 rows and 5 variables.
+NULL
