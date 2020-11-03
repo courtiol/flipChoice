57ff9ff338eca9dbf4a4da820bb21994c5deb8cd
Alexandre Courtiol <alexandre.courtiol@gmail.com>
2020-11-03 21:58:03 +0100

    Restore fully documentation

--- R/data.R
+++ R/data.R
@@ -7,7 +7,13 @@ NULL
 
 #' Choice Design from Huber and Zwerina
 #' A dataset containing a choice model experimental design chosen to minimize D-error used in an example in Huber and Zwerina (1996).
-#' @references Joel Huber and Klaus Zwerina (1996). The Importance of Utility Balance in Efficient Choice Designs. Journal of MArketing Research.
+#'
+#' - Question. Question/Task number in the experiment
+#' - Alternative. Alternative number within a question
+#' - Attribute_1. The first attribute in the design, consisting of three levels.
+#' - Attribute_2. The second attribute in the design, consisting of three levels.
+#' - Attribute_3. The third attribute in the design, consisting of three levels.
+#' @references Joel Huber and Klaus Zwerina (1996). The Importance of Utility Balance in Efficient Choice Designs. Journal of Marketing Research.
 #' @docType data
 #' @name hz.design
 #' @usage data(hz.design)
